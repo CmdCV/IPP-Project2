417bf588e971d9778b3a632af776257ff79cf7b6
Ale? Urb?nek <xurbana00@vutbr.cz>
2025-04-11 19:30:25 +0000

    Test script

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+for i in {1..38}; do
+    OUTPUT=$(php interpret.php --source="./student/inputs/$i.xml" 2>&1)
+    if [ $? -eq 0 ]; then
+        echo -e "${i} ${GREEN}SUCCESS${RESET}"
+    else
+        echo -e "${i} ${RED}FAILED${RESET}"
+        echo "$OUTPUT" | sed 's/^/    /'
+    fi
+done
